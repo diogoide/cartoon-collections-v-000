8d69d4cc6fcc6b2e00fd4acac718f9365650062a
Diogo Ide <diogoide@gmail.com>
2018-04-06 00:19:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,5 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
-
+  return array.find {|i| i == "cheddar" || "gouda" || "camembert"}
 end
