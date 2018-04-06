669787bbb8d3a267c08507fa8378709d046fec4d
Diogo Ide <diogoide@gmail.com>
2018-04-06 00:02:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)# code an argument here
   array.map { |word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? {array.length > 4} 
 end
 
 def find_the_cheese# code an argument here
