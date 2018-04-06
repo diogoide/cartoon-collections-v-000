7c49ab13d08aefbc4ae284a16ba4fdd9cee63d83
Diogo Ide <diogoide@gmail.com>
2018-04-06 00:05:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.any? {array.length > 4} 
+  array.any? {array.length > 4}
 end
 
 def find_the_cheese(array)# code an argument here
