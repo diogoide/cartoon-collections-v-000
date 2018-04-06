4fd912cea8b016120e66bd165f2203b5c4ae5574
Diogo Ide <diogoide@gmail.com>
2018-04-06 00:35:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,7 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {array.length > 4}
 end
 
-def find_the_cheese(array)# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  return array.find {|i| i == "cheddar" || "gouda" || "camembert"}
+  array.find {|i| i == "cheddar" || i == "gouda" || i == "camembert"}
 end
